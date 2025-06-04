e044fe0ee585ddab2d9be81c36cd8c93326c6365
rubenalfredoperazatorres <rubenperazatorres@gmail.com>
2025-06-03 19:39:57 -0600

    Cambios en app.py para agregar la clase anfis_model

--- app.py
+++ app.py
@@ -1,29 +1,12 @@
+from flask import Flask, render_template, request, jsonify
 import torch
-import torch.nn as nn
 import joblib
 import numpy as np
-from flask import Flask, render_template, request, jsonify
+from anfis_model import ANFIS  
 
 app = Flask(__name__)
 
-# Clase del modelo ANFIS (debe coincidir con la usada en el entrenamiento)
-class ANFIS(nn.Module):
-    def __init__(self, n_inputs, n_rules):
-        super(ANFIS, self).__init__()
-        self.n_inputs = n_inputs
-        self.n_rules = n_rules
-        self.pesos = nn.Parameter(torch.randn(n_rules, n_inputs))  
-        self.membresia = nn.Parameter(torch.randn(n_rules, 1))  
-
-    def forward(self, x):
-        reglas = torch.matmul(x, self.pesos.T)  
-        reglas_activadas = torch.softmax(reglas, dim=1)  
-        membresia_activada = torch.matmul(reglas_activadas, self.membresia)  
-        return membresia_activada  
-
-# Cargar el modelo ANFIS y el scaler
-torch.serialization.add_safe_globals([ANFIS])
-# model = torch.load("anfis_modelo_27.pth", weights_only=False)
+# Cargar modelo y scaler
 model = torch.load("anfis_modelo_27.pth", map_location=torch.device('cpu'))
 model.eval()
 scaler = joblib.load("scaler27.pkl")
@@ -35,17 +18,15 @@ def index():
 @app.route('/predecir', methods=['POST'])
 def predecir():
     try:
-        # Obtener datos del formulario
         datos = [float(request.form[key]) for key in request.form]
-        datos_entrada = np.array([datos])  # Convertir a numpy array
-        datos_entrada = scaler.transform(datos_entrada)  # Escalar datos
+        datos_entrada = np.array([datos])
+        datos_entrada = scaler.transform(datos_entrada)
 
-        # Convertir a tensor y predecir
         input_tensor = torch.tensor(datos_entrada, dtype=torch.float32)
         output = model(input_tensor)
-        probabilidad = torch.sigmoid(output).item() * 100  # Convertir a porcentaje
+        probabilidad = torch.sigmoid(output).item() * 100
 
-        return jsonify({"probabilidad": round(probabilidad, 2)})  # Responder en JSON
+        return jsonify({"probabilidad": round(probabilidad, 2)})
     except Exception as e:
         return jsonify({"error": str(e)})
 
